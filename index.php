fe89cff8d3a629e9582e8ba4892206acff971271
thuongdv_58 <thuongdv_58@vnu.edu.vn>
2016-01-25 21:06:30 +0700

    he

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>my personal site</title>
+    <script src = "http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+</head>
+<body>
+<h1>sdjhdgsjsdftgjwsjnhrtgsjn</h1>
+<?php
+	echo "<p>hayhay</p>"
+?>
+</body>
+</html>
