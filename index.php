f7282d4d68cbd81f7bfdb367dc7dce92d1030171
thuongdv_58 <thuongdv_58@vnu.edu.vn>
2016-01-27 23:14:37 +0700

    profile added

--- index.php
+++ index.php
@@ -6,9 +6,14 @@
     <script src = "http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
 </head>
 <body>
-<h1>sdjhdgsjsdftgjwsjnhrtgsjn</h1>
+<h1>Personal webpage</h1>
+<div class="profile">
+	<div class="name">Đinh Viết Thường</div>
+	<div class="class">K58CA</div>
+	<div class="born">17/10/1995</div>
+</div>
 <?php
-	echo "<p>hayhay</p>"
+	echo "<p></p>"
 ?>
 </body>
 </html>
