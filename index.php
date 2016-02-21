5ef77e950e4f3c31286aa0ad3c9e105c4259b7c7
thuongdv_58 <thuongdv_58@vnu.edu.vn>
2016-02-21 19:53:57 +0700

    update website

--- index.php
+++ index.php
@@ -8,9 +8,14 @@
 <body>
 <h1>Personal webpage</h1>
 <div class="profile">
-	<div class="name">Đinh Viết Thường</div>
-	<div class="class">K58CA</div>
-	<div class="born">17/10/1995</div>
+	<div class="name">Name :Đinh Viết Thường</div>
+	<div class="class">classname :K58CA</div>
+    <div class="school">School: University of Engineering and Technology</div>
+    <div class="major">Major: Computer Science</div>
+    <div class="que">Hometown:Hải Phòng</div>
+	<div class="born">Ngày sinh :17/10/1995</div>
+    <div class="phong">phone: 0965940059</div>
+    <div class="mail">mail :thuongdv_58@vnu.edu.vn</div> 
 </div>
 <?php
 	echo "<p></p>"
